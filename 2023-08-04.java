75f609656f15c4f7ef7faf6d21877213071374fe
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-08-04 17:50:50 +0800

    Update 2023-08-04.java

--- 2023-08-04.java
+++ 2023-08-04.java
@@ -1,1 +1,10 @@
+public class Circle {
+    public static final double PI = 3.14159;
+    public static double calculateArea(double radius) {
+        return PI * radius * radius;
+    }
 
+    public void main(string[] args) {
+        // do sth for java practice
+    } 
+}
