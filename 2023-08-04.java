d44c11fabfc8b4657f876417b4863b421a56190b
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-08-04 17:51:14 +0800

    Update 2023-08-04.java

--- 2023-08-04.java
+++ 2023-08-04.java
@@ -1,5 +1,6 @@
 public class Circle {
     public static final double PI = 3.14159;
+    // js const PI = 3.14159
     public static double calculateArea(double radius) {
         return PI * radius * radius;
     }
